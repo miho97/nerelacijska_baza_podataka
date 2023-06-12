c65a364beb81512ba4b3a7cf1c841b8086f0d4db
Mihovil Stručić <mihovil.strucic@gmail.com>
2023-06-12 12:20:32 +0200

    dodano

--- lekser.py
+++ lekser.py
@@ -7,14 +7,22 @@ from vepar import *
 
 class T(TipoviTokena):
 
-    MATCH, WITH, WHERE, CALL = 'match','with','where','call'
-    RETURN,AS = 'return','as'
+    MATCH, WITH, WHERE, CALL = 'MATCH','WITH','WHERE','CALL'
+    RETURN,AS = 'RETURN','AS'
 
-    OPEN, CLOSED, COLON, SEMICOLON = '(),;'
-    COPEN, CCLOSED = '}{'
+    FOR, IF, MAIN, PRINT = 'for','if','main', 'print'
+
+    OPEN, CLOSED, COLON, SEMICOLON, EQUAL, LESS = '(),;=<'
+    CCLOSED, COPEN = '}{'
     PLUS, PUTA, = '+*'
+    EEQUAL = '=='
+    PLUSJ = '+='
 
     class NODE(Token):... 
+    class IME(Token):
+        def vrijednost(t): return rt.symtab[t]
+    class BROJ(Token):
+        def vrijednost(t): return int(t.sadržaj)
     #class KEYWORD(Token):pass
 
 
@@ -61,33 +69,66 @@ def lekser(lex):
                     lex * str.isdecimal
                 yield lex.token(T.NODE)
             else: yield lex.token(T.NODE)
-         
-       
+
+        # ključne riječi oznančene isključivo malim slovima i imena varijabli, zasad neku su varijable a,b,c,x,y,z,...
+            
+        elif znak.isalpha and znak.islower():
+            if lex > str.isalpha:
+                lex * str.isalpha
+                if lex.sadržaj == 'main':
+                    lex >> '('
+                    lex >> ')'
+                    yield lex.token(T.MAIN)
+                elif lex.sadržaj == 'for':
+                    yield lex.token(T.FOR)
+                elif lex.sadržaj == 'if':
+                    yield lex.token(T.IF)
+                #else:
+                #    yield lex.literal(T)
+            else:
+                yield lex.token(T.IME)
+
+        elif znak == '+':
+            if lex >= '=':
+                yield lex.token(T.PLUSJ)
         elif znak == '(':
             yield lex.token(T.OPEN)
         elif znak == ',':
             yield lex.token(T.COLON)
         elif znak == ')':
             yield lex.token(T.CLOSED)
+        elif znak == '{':
+            yield lex.token(T.COPEN)
+        elif znak == '}':
+            yield lex.token(T.CCLOSED)
         elif znak == ';':
             yield lex.token(T.SEMICOLON)
         elif znak == '/':   ## jednolinijski komentari neka budu oznaceni sa //
             lex >> '/'
             lex - '\n'
             lex.zanemari()
+        elif znak.isdecimal():
+            lex.prirodni_broj(znak)
+            yield lex.token(T.BROJ)
+     
 
+# pogledati sto cemo sa TIJELO i NAREDBA
         
-        """
-        elif znak == '(':
-            drugo = next(lex)
-            if drugo == ')': yield lex.token(T.NEIGHBOURS)
-            elif drugo.isalpha:
-                if not drugo.isupper(): raise lex.greška('NODE nije označen sukladno pravilima')
-                prvo = next(lex)
-                if prvo != '0':
-                    lex * str.isdecimal
-                yield lex.token()
-        """
+### pocetna BKG
+##  program -> main |  funkcija program         # tu mozemo dodat proizvoljno mnogo funkcija prije ulaska u main
+##  funkcija -> ime OPEN parametri CLOSED  COPEN TIJELO CCLOSED         # klasicna definicija funkcije
+##  parametri -> ime  |  parametri COLON ime
+##  TIJELO   ->  deklaracija TIJELO | NAREDBA
+##  main  -> deklaracija main | NAREDBA main | NAREDBA
+##  NAREDBA  ->  petlja  |  grananje   |  ispis
+##  petlja   ->  for COPEN  NAREDBA CCLOSED
+##  for   ->  FOR OPEN IME# EQUAL BROJ SEMICOLON IME# LESS BROJ SEMICOLON inkrement CLOSED
+##  inkrement -> PLUSJ BROJ
+##  ispis ->  PRINT  OPEN varijabla CLOSED
+
+
+
+## ISPROBAVANJE LEKSERA
 
 #ulaz = 'A12(B,C,E),B(),C(F)//par nodeova grafa su dodani\n  F(G,A,H,I)'
 #ulaz = 'A1B2'
@@ -95,13 +136,63 @@ def lekser(lex):
 #ulaz = 'MATCH A RETURN A'
 ulaz = '''
 
-MATCH A RETURN A
-B(C,D,E),D(E),G(H,A,I,J);
+main(){
+    MATCH A RETURN A
+    for(i = 0; i < 10; i += 1)
 
+}
 '''
 lekser( ulaz )
 
-##ulaz2 = 'A23sd'
-##program(ulaz2)
+ulaz2 = '''
+
+funkcija()
+
+main(){
+
+    print(x)
 
 
+}
+
+
+'''
+
+class P(Parser):
+    def program(p) -> 'Memorija':
+        p.funkcije = Memorija()  # redefinicija = False?
+        while not p >= T.MAIN:
+            funkcija = p.funkcija()
+            p.funkcije[funkcija.ime] = funkcija
+        p.main()
+        return p.funkcije
+    def main(p) :
+        p.naredba()
+
+    def naredba(p):
+        p >= T.PRINT
+        return p.ispis()
+    def ispis(p):
+        p >= T.PRINT
+        p >= T.OPEN
+        p >= T.PRINT
+        p >= T.CLOSED
+        return T.IME.vrijednost()
+    def funkcija(p)-> 'Funkcija':
+        atributi = p.imef, p.parametrif = p.ime(), p.parametri()
+        p >= T.COPEN
+        nesto = Funkcija(*atributi, p.naredba())
+        p >= T.CLOSED
+        return nesto
+    #def parametri(p):...
+
+prikaz( P(ulaz2))
+
+class Funkcija(AST):
+    ime: 'IME'
+    parametri: 'IME*'
+    tijelo: 'naredba'
+    def pozovi(funkcija, argumenti):
+
+class Parametri(AST):...
+print( help(Memorija))
